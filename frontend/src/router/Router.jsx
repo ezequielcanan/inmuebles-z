20a4e20e6c1ed7572c4ffb9f6777bb5bfaa8cdfd
Ezequiel Canan <ezequielcanan@gmail.com>
2024-01-05 17:36:34 -0300

    Init

---
+++ frontend/src/router/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Router = () => {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"/>
+      <Route path="/new-project"/>
+      <Route path="/category/:category"/>
+      <Route path="/:proyect"/>
+      <Route path="/:proyect/edit"/>
+      <Route path="/:proyect/:piso"/>
+      <Route path="/:proyect/:piso/:inmueble"/>
+      <Route path="/:proyect/:piso/:inmueble/edit"/>
+      <Route path="/login"/>
+      <Route path="/register"/>
+    </Routes>
+  </BrowserRouter>
+}
+
+export default Router
