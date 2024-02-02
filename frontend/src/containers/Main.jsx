4d176d0d5a78d2425253a51a2e1d6da3a3df86a6
Ezequiel Canan <ezequielcanan@gmail.com>
2024-02-02 19:44:59 -0300

    separating project

--- frontend/src/containers/Main.jsx
+++
@@ -1,9 +0,0 @@
-const Main = ({children, className}) => {
-  return (
-    <main className={"min-h-screen flex flex-col px-20 " + className}>
-      {children}
-    </main>
-  )
-}
-
-export default Main
