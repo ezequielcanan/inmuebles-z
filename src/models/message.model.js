de0fb29d9ec2fb4ff7a49422e425255bdfcaf43f
unknown <ezequielcanan@gmail.com>
2024-03-03 21:57:30 -0300

    Notifications

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -6,7 +6,9 @@ const messagesSchema = new mongoose.Schema({
   dateTime: Date,
   from: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
   to: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
-  text: String
+  text: String,
+  type: String,
+  data: Object
 })
 
 export default mongoose.model(messagesCollection, messagesSchema)
--- src/sockets.js
+++ src/sockets.js
@@ -6,8 +6,10 @@ export default io => {
       const uid = user._id
       socket.join(uid)
 
-      const userMessages = await messageModel.find({ to: uid }).lean().exec()
-      socket.emit("messages", userMessages)
+      socket.on("getMessages", async () => {
+        const userMessages = await messageModel.find({ to: uid }).lean().exec()
+        socket.emit("messages", userMessages)
+      })
 
       socket.on("sendMessage", async ({ message, receiver }) => {
         const result = await messageModel.create(message)
